e436eac2fddc7e21ad638492a8fb393fe65bedf1
Dannas Cornell <dannas.cornell@gmail.com>
2020-10-29 11:25:18 -0600

    Update perimetro_triangulo.c
    Se corrigió la línea 11: la función printf se escribió correctamente y se agrego la impresión del resultado.

--- perimetro_triangulo.c
+++ perimetro_triangulo.c
@@ -7,8 +7,8 @@ float resultado;
       
 	  printf( "Ingresa los lados del triangulo para calcular el perimetro: ");
 scanf( "%d", &lado );
-resultado = lado * (3);
-prinft( "\nEl perimetro de un triangulo equilatero de %d centimetros por lado da por resultado: ", lado, resultado);
+resultado = lado * 3;
+printf( "\nEl perimetro de un triangulo equilatero de %d centimetros por lado da por resultado: %.2f\n", lado, resultado);
 
 return 0;
 }
